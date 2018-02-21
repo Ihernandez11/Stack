429e6ce3c445381b36d7c0eb38154fa9f2f47977
ihernandez11 <35441196+Ihernandez11@users.noreply.github.com>
2018-02-20 21:50:44 -0600

    Add project files.

---
+++ StackProject/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new StackProject();
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+
+            //stack.Clear();
+
+        }
+    }
+}
---
+++ StackProject/StackProject.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackProject
+{
+    class StackProject : Stack<object>
+    {
+        /*public object Obj { get; set; }
+
+        public void Push(object obj)
+        {
+            
+            //if obj is null, throw new exception
+            if(obj == null)
+            {
+                throw new InvalidOperationException();
+            }
+            else
+            {
+                Obj = obj;
+            }
+
+        }*/
+
+        
+    }
+}
